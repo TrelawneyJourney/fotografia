c3a07949a6b9a63547c4f88c514a084c724fbe4e
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-12-10 00:15:09 -0300

    rutas de img

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -3,31 +3,31 @@ export const photos = [
     id: 1,
     titulo: "Medano Blanco",
     slug: "medano-blanco",
-    portada: "/photos/1.jpg",
+    portada: `${import.meta.env.BASE_URL}photos/1.jpg`,
   },
   {
     id: 3,
     titulo: "Atardecer en la playa",
     slug: "atardecer-en-la-playa",
-    portada: "/photos/3.jpg",
+    portada: `${import.meta.env.BASE_URL}photos/3.jpg`,
   },
   {
     id: 5,
     titulo: "Noche en la playa",
     slug: "noche-en-la-playa",
-    portada: "/photos/5.jpg",
+    portada: `${import.meta.env.BASE_URL}photos/5.jpg`,
   },
   {
     id: 6,
     titulo: "Buzios",
     slug: "buzios",
-    portada: "/photos/6.jpg",
+    portada: `${import.meta.env.BASE_URL}photos/6.jpg`,
   },
   {
     id: 8,
     titulo: "En algun lado",
     slug: "pato",
-    portada: "/photos/8.jpg",
+    portada: `${import.meta.env.BASE_URL}photos/8.jpg`,
   },
 ];
 
