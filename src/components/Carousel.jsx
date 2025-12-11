98a2672e3674c1a635df2dfff53077a9b67840b3
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-12-11 20:12:28 -0300

    ajuste overlay

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -32,7 +32,7 @@ export default function Carousel() {
                 alt={ph.titulo}
                 className="w-full h-full object-cover cursor-pointer"
               />
-              <div className="absolute bottom-8 md:bottom-5 left-10">
+              <div className="relative z-20 flex h-full items-end pl-10 pb-5">
                 <PhotoDescription description={ph.titulo} />
               </div>
             </div>
