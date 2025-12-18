6427a0b88fa932d58ed4c62da3b14205e6a748ff
TrelawneyJourney <the-trelawney-journey@outlook.com>
2025-12-18 13:17:04 -0300

    scroll projects

--- src/components/CarouselProjects.jsx
+++ src/components/CarouselProjects.jsx
@@ -1,67 +1,74 @@
-import { useEffect, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Mousewheel } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/pagination";
+import { useState, useEffect, useRef } from "react";
+import VerticalPagination from "./VerticalPagination";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 import { proyectos } from "../data/Projects";
 
-gsap.registerPlugin(ScrollTrigger);
-
-export default function CarouselProjects() {
-  const stackRef = useRef([]);
+export default function Carousel() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const slidesRef = useRef([]);
 
   useEffect(() => {
-    stackRef.current.forEach((item, i) => {
-      if (i === 0) return;
-
+    slidesRef.current.forEach((slide) => {
+      const image = slide.querySelector(".slide-image");
       gsap.fromTo(
-        item,
+        image,
+        { opacity: 0, scale: 1.05, y: 40 },
         {
-          y: "100%",
-          scale: 1.05,
-        },
-        {
-          y: "0%",
+          opacity: 1,
           scale: 1,
-          ease: "none",
+          y: 0,
+          duration: 1.2,
+          ease: "power3.out",
           scrollTrigger: {
-            trigger: document.body,
-            start: `${i * 100}vh top`,
-            end: `${(i + 1) * 100}vh top`,
-            scrub: true,
+            trigger: slide,
+            start: "top center",
+            toggleActions: "play none none reverse",
           },
         }
       );
     });
-
     return () => ScrollTrigger.killAll();
   }, []);
 
   return (
     <>
-      {/*  scroll */}
-      <div
-        className="absolute top-0 left-0 w-full"
-        style={{ height: `${proyectos.length * 100}vh` }}
-      />
+      <VerticalPagination active={activeIndex} total={proyectos.length} />
 
-      {/* fixed scene */}
-      <div className="fixed inset-0 flex items-center justify-center pointer-events-none">
-        <div className="relative w-[800px] h-[400px]">
-          {proyectos.map((ph, index) => (
-            <div
-              key={ph.id}
-              ref={(el) => (stackRef.current[index] = el)}
-              className="absolute inset-0"
-              style={{ zIndex: proyectos.length - index }}
-            >
-              <img
-                src={ph.portada}
-                alt={ph.titulo}
-                className="w-full h-full object-cover"
-              />
+      <Swiper
+        rewind={true}
+        mousewheel={{ forceToAxis: true }}
+        speed={800}
+        threshold={5}
+        direction="vertical"
+        modules={[EffectFade, Mousewheel]}
+        className="mySwiper h-screen w-full"
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+      >
+        {proyectos.map((ph, index) => (
+          <SwiperSlide
+            key={ph.id}
+            className="slide"
+            ref={(el) => (slidesRef.current[index] = el)}
+          >
+            <div className=" h-full w-full flex items-center justify-center">
+              <div className="w-[800px] h-[400px] slide_inner">
+                <img
+                  src={ph.portada}
+                  alt={ph.titulo}
+                  className="slide-image w-full h-full object-cover cursor-pointer"
+                />
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,21 +1,23 @@
-import { proyectos } from "../data/Projects";
 import CarouselProjects from "../components/CarouselProjects";
+import { proyectos } from "../data/Projects";
 
 export default function Projects() {
   return (
-    <div className="relative min-h-screen bg-gradient-to-b from-neutral-200 to-stone-50">
-      {/* menu */}
-      <aside className="fixed left-0 bottom-10 z-50 bg-amber-300 hidden md:block pl-14">
-        <ul className="space-y-2">
+    <main className="bg-gradient-to-b from-neutral-200 to-stone-50">
+      <aside className="fixed left-0 bottom-10 z-40 hidden md:block pl-14">
+        <ul className="space-y-1">
           {proyectos.map((item) => (
-            <li key={item.id}>{item.titulo}</li>
+            <li
+              key={item.id}
+              className="text-xl text-neutral-600 hover:text-neutral-950 cursor-pointer"
+            >
+              {item.titulo}
+            </li>
           ))}
         </ul>
       </aside>
 
-      {/* imagen */}
-
       <CarouselProjects />
-    </div>
+    </main>
   );
 }
